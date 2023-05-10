e55f00387a072f80bc100f74440fdb58a3aa2573
imerla1 <giorgi1048@yahoo.com>
2023-05-10 17:41:38 +0400

    this keyword

--- 03_js_behind_scene/thiskw.js
+++ 03_js_behind_scene/thiskw.js
@@ -1,34 +1,45 @@
-console.log(this)
+console.log(this);
 
 const calcAge = function (birthYear) {
-    console.log(2037 - birthYear)
-    console.log(this)
-}
+  console.log(2037 - birthYear);
+  console.log(this);
+};
 
-const calcAgeArrow = birthYear => {
-    console.log(2037 - birthYear)
-    console.log(this)
-}
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
 
 const jonas = {
-    year: 1991,
-    calcAge: function(){
-        console.log(this)
-    }
-}
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+  },
+};
 
 const george = {
-    year: 1999
-}
+  year: 1999,
+};
 
-george.calcAge = jonas.calcAge
+george.calcAge = jonas.calcAge;
 
-george.calcAge()
+george.calcAge();
 
+const james = {
+  year: 1992,
+  calcAge: function () {
+    console.log(`age ${2037 - this.year}`);
+    const isMillenial = () => {
+      console.log(this.year >= 1991 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+};
 
-function Person(name) {
-    this.name = name;
-  }
-  
-  const alice = new Person("Alice");
-  console.log(alice.name); // logs "Alice"
+james.calcAge();
+
+const addExpr = function (a, b) {
+    console.log(arguments)
+};
+
+addExpr(1,2,3)
