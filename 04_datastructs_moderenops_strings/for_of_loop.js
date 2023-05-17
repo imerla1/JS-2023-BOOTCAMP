5c135002afe0ec4bd29acf27f8cd86ab641c9064
imerla1 <giorgi1048@yahoo.com>
2023-05-17 15:14:24 +0400

    for of loop

---
+++ 04_datastructs_moderenops_strings/for_of_loop.js
@@ -0,0 +1,50 @@
+const array = [1, 2, 3, 4, 5];
+
+for (const item in array) {
+  console.log(item);
+}
+
+const game = {
+    team1: "Bayern Munich",
+    team2: "Borrussia Dortmund",
+    players: [
+      [
+        "Neuer",
+        "Pavard",
+        "Martinez",
+        "Alaba",
+        "Davies",
+        "Kimmich",
+        "Goretzka",
+        "Coman",
+        "Muller",
+        "Gnarby",
+        "Lewandowski",
+      ],
+      [
+        "Burki",
+        "Schulz",
+        "Hummels",
+        "Akanji",
+        "Hakimi",
+        "Weigl",
+        "Witsel",
+        "Hazard",
+        "Brandt",
+        "Sancho",
+        "Gotze",
+      ],
+    ],
+    score: "4:0",
+    scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+    date: "Nov 9th, 2037",
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+  };
+
+for (const [k, v] of game) {
+    console.log(k,v)
+}
